bebee67b759360d7b2c82e11baf617a4b54d30a0
portco <woosubc@gmail.com>
2014-11-19 22:33:25 -0800

    Update registrations_controller.rb

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,10 +3,10 @@ class RegistrationsController < Devise::RegistrationsController
   private
   # Overriding so registrations controller allows for different user types
   def sign_up_params
-    params.require(:user).permit(:type, :email, :password, :password_confirmation)
+    params.require(:user).permit(:role, :email, :password, :password_confirmation)
   end
  
   def account_update_params
-    params.require(:user).permit(:type, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:role, :email, :password, :password_confirmation, :current_password)
   end
 end
