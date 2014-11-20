f47696103bb1bd7c8820dda7c7d7661c288ee6e7
portco <woosubc@gmail.com>
2014-11-19 22:54:09 -0800

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20141111043608) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "type",                                null: false
+    t.string   "role",                                null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
