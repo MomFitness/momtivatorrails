d9020d17d8489afc7dbba19cdab06023ab627f0d
portco <woosubc@gmail.com>
2014-11-19 22:53:51 -0800

    Update 20141111043608_add_type_to_users.rb

--- db/migrate/20141111043608_add_type_to_users.rb
+++ db/migrate/20141111043608_add_type_to_users.rb
@@ -1,5 +1,5 @@
-class AddTypeToUsers < ActiveRecord::Migration
+class AddRoleToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :type, :string, :null => false
+    add_column :users, :role, :string, :null => false
   end
 end
