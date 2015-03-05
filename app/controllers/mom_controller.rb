5754d3c7d88bced866829d5738572e76e1e39f4c
Jay Chung <woosubc@gmail.com>
2015-03-04 21:54:36 -0800

    Fix the default behavior of mom's dashboard.
    * app/controllers/mom_controller.rb
      change the default behavior of mom's dashboard.

--- app/controllers/mom_controller.rb
+++ app/controllers/mom_controller.rb
@@ -4,7 +4,7 @@ class MomController < ApplicationController
   before_filter :required_mom
   
   def dashboard
-    @workouts = params[:filter] == "all" ? current_user.mom_workouts : current_user.mom_workouts.where(:workout_date => Date.today)
+    @workouts = params[:filter] && params[:filter] == "all" ? current_user.mom_workouts : current_user.mom_workouts.where(:workout_date => Date.today)
   end
   
 end
